d84c00dc4363de00036404b302f4839dcf457140
nulysses <adr.zivil@gmail.com>
2021-04-28 15:29:34 +0200

    1

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ "$(whoami)" != "root" ]]; then
+  read -p "[ERROR] must be root!"
+  exit 1
+fi
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+. "${script_dir}/vars"
+
+if [ ! -d "${install_dir}" ]; then
+  mkdir -p "${install_dir}"
+fi
+
+rm -rf "${install_dir}/${module_name}"
+cp -rf "${script_dir}/${module_name}" "${install_dir}/${module_name}"
