cac16055536f3026a3497effee8e82606bdfec89
nulysses <adr.zivil@gmail.com>
2021-06-19 15:28:34 +0200

    1

--- xfce_keyboard_shortcuts/exec.sh
+++ xfce_keyboard_shortcuts/exec.sh
@@ -32,18 +32,13 @@ set -o pipefail
 set -eu
 #################################SCRIPT_START##################################
 
-. "${script_dir}/vars"
 if [ ! -v ar18_helper_functions ]; then rm -rf "/tmp/helper_functions_$(whoami)"; cd /tmp; git clone https://github.com/ar18-linux/helper_functions.git; mv "/tmp/helper_functions" "/tmp/helper_functions_$(whoami)"; . "/tmp/helper_functions_$(whoami)/helper_functions/helper_functions.sh"; cd "${script_dir}"; export ar18_helper_functions=1; fi
 obtain_sudo_password
-
-ar18_install "${install_dir}" "${module_name}" "${script_dir}"
 
 set +u
 ar18_deployment_target="$(read_target "${1}")"
 set -u
-
-echo "${ar18_sudo_password}" | sudo -Sk chmod +x "${script_dir}/${module_name}/exec.sh"
-"${script_dir}/${module_name}/exec.sh" "${ar18_deployment_target}"
+source_or_execute_config "execute" "xfce_keyboard_shortcuts" "${ar18_deployment_target}"
 
 ##################################SCRIPT_END###################################
 # Restore old shell values
